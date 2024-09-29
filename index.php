63ab1b7aa6f1843034c9f48cd323daefc997d4af
Giancarlos1024 <giancarlosvlsqz@gmail.com>
2024-09-28 23:27:17 -0500

    footer terminado

--- index.php
+++ index.php
@@ -23,6 +23,46 @@
     </header>
     <h1>Hola</h1>
 
-    <?php include('templates/footer.php');?>
+    <footer>
+        <section class="SeccionFooter-One">
+            
+            <div class="LogoFooter">
+                <img src="./public/img/LogoFooter.png" alt="Logo Header">
+            </div>
+            <div class="informacionFooter">
+                <h4>Informacion de contacto</h4>
+                <div>
+                    <img src="./public/img/phone.png" alt="phone">
+                    <a href="#" target="_blank">+51 923 217 317</a>
+                </div>
+                <div>
+                    <img src="./public/img/mensaje.png" alt="mensaje">
+                    <a href="#" target="_blank">contacto@freyescorredoresseguros.com</a>
+                </div>
+                <div>
+                    <img src="./public/img/ubicacion.png" alt="ubicacion">
+                    <a href="#" target="_blank">Residencial San Martín G-101, Ica, Perú</a>
+                </div>   
+            </div>
+            <div>
+            <div class="redes">
+                <h4>Siguenos en :</h4>
+                <div>
+                    <a href="#"><img src="./public/img/facebook.png" alt="facebook"></a>
+                    <a href="#"><img src="./public/img/instagram.png" alt="instagram"></a>
+                </div> 
+
+            </div>
+            
+        </section>
+        <section class="SeccionFooterLinea">
+            <div></div>
+        </section>
+        <section class="SeccionFooter-Two">
+            <div>
+                <p>® 2​023 - F. REYES Y CIA  Corredores de Seguros - Todos los derechos reservados.</p>
+            </div>
+        </section>
+    </footer>
 </body>
 </html>
--- templates/footer.php
+++ templates/footer.php
@@ -5,12 +5,50 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="../public/css/footer.css">
+    <link rel="stylesheet" href="../public/css/style.css">
     <title>Document</title>
 </head>
 <body>
     <footer>
-        <img src="./public/img/LogoFooter.png" alt="Logo Header">
-        
+        <section class="SeccionFooter-One">
+            
+            <div class="LogoFooter">
+                <img src="../public/img/LogoFooter.png" alt="Logo Header">
+            </div>
+            <div class="informacionFooter">
+                <h4>Informacion de contacto</h4>
+                <div>
+                    <img src="../public/img/phone.png" alt="phone">
+                    <a href="#" target="_blank">+51 923 217 317</a>
+                </div>
+                <div>
+                    <img src="../public/img/mensaje.png" alt="mensaje">
+                    <a href="#" target="_blank">contacto@freyescorredoresseguros.com</a>
+                </div>
+                <div>
+                    <img src="../public/img/ubicacion.png" alt="ubicacion">
+                    <a href="#" target="_blank">Residencial San Martín G-101, Ica, Perú</a>
+                </div>   
+            </div>
+            <div>
+            <div class="redes">
+                <h4>Siguenos en :</h4>
+                <div>
+                    <a href="#"><img src="../public/img/facebook.png" alt="facebook"></a>
+                    <a href="#"><img src="../public/img/instagram.png" alt="instagram"></a>
+                </div>   
+
+            </div>
+            
+        </section>
+        <section class="SeccionFooterLinea">
+            <div></div>
+        </section>
+        <section class="SeccionFooter-Two">
+            <div>
+                <p>® 2​023 - F. REYES Y CIA  Corredores de Seguros - Todos los derechos reservados.</p>
+            </div>
+        </section>
     </footer>
 </body>
 </html>
